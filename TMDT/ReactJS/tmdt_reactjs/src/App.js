7b0acdace99aed37949d268b9727f4057c06108d
Tung Nguyen <nguyenvansontungvt113@gmail.com>
2022-12-16 16:31:48 +0700

    Complete Add product, view my product

--- TMDT/ReactJS/tmdt_reactjs/src/App.js
+++ TMDT/ReactJS/tmdt_reactjs/src/App.js
@@ -11,18 +11,24 @@ import UserScore from './Jsx/Score';
 import MyProduct from './Jsx/My_product';
 import MyProductItem from './Jsx/My_product_item';
 import ViewMyProduct from './Jsx/View-my-product';
+import WonProduct from './Jsx/Won_product';
+import AddProduct from './Jsx/Add_product';
+import Cart from './Jsx/Cart';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Cart" element={<Cart />} />
       <Route path="/Products/:id" element={<ProductDetail />} />
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/Navbar" element={<ProfileNavbar />} />
       <Route path="/Profile" element={<Profile />} />
       <Route path="/ChangePassWord" element={<ChangePassWord />} />
+      <Route path="/WonProducts" element={<WonProduct />} />
       <Route path="/UserScore" element={<UserScore />} />
       <Route path="/MyProducts" element={<MyProduct />} />
+      <Route path="/AddProduct" element={<AddProduct />} />
       <Route path="/ViewMyProduct" element={<ViewMyProduct />} />
       <Route path="/MyProductItem" element={<MyProductItem />} />
     </Routes>
---
+++ TMDT/ReactJS/tmdt_reactjs/src/Jsx/Won_product_item.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import '../Css/Base.css';
+import '../Css/Grid.css';
+import '../Css/Main.css';
+import '../Css/My_product_item.css';
+import '../Css/Won_product_item.css';
+import Header from './Header';
+import Footer from './Footer';
+import SpecialBtn from './Special_btn';
+import ProductItem from './Product_item';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { Link } from 'react-router-dom';
+
+// import { Carousel, CarouselItemProps } from 'react-bootstrap'
+import 'react-multi-carousel/lib/styles.css';
+import { useEffect } from 'react';
+import ProfileNavbar from './Profile_navbar';
+
+function MyProductItem(props) {
+  return (
+    <>
+      <Link to="/" className="column l-12 won-product-item-container">
+        <div
+          className={`column l-2 won-product-item__img ${props.className}`}
+          style={{
+            backgroundImage: `url(${props.url})`,
+          }}
+        ></div>
+        <div className="column l-8 won-product-info-container">
+          <h3 className="won-product-heading">{props.heading}</h3>
+          <p className="won-product-description">{props.description}</p>
+        </div>
+        <div className="column l-2 view-won-product-item__btn-container">
+          <SpecialBtn className="view-won-product-item__btn" value="View" />
+        </div>
+      </Link>
+    </>
+  );
+}
+
+export default MyProductItem;
